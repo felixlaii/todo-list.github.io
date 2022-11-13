1e81202d13058c8a0a7e9436797f5204c380e3fe
Felix Lai <felix.lai@hotmail.com>
2022-11-13 10:36:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ const App = () => {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
   const [editId, setEditId] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) => t.id === editTodo.id ? (t = { id: t.id, todo}) : {id: t.id, todo: t.id});
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+  }
   return (
     <div className="App">
     </div>
