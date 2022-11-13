8311c1ddf27e3b59f9428b14738add0a75c9b0b1
Felix Lai <felix.lai@hotmail.com>
2022-11-13 10:34:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
   return (
     <div className="App">
     </div>
