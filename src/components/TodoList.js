bdae3030195cdcb7479c39e9c4949cca177e31a4
Felix Lai <felix.lai@hotmail.com>
2022-11-13 10:32:23 -0500

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,19 @@
-import Reach from 'react';
+import Reach from "react";
 
 const TodoList = ({ todos, handleDelete, handleEdit }) => {
-    return (
-        <ul className="allTodos">
-            {todos.map((t) => (
-                <li className='singleTodo'>
-                    <span className='todoText' key={t.id}>
-                        {t.todo}
-                    </span>
-                    <button onClick={() => handleEdit(t.id)}>Edit</button>
-                    <button onClick={() => handleDelete(t.id)}>Delete</button>
-                </li>
-            ))}
-        </ul>
-    )
-}
+  return (
+    <ul className="allTodos">
+      {todos.map((t) => (
+        <li className="singleTodo">
+          <span className="todoText" key={t.id}>
+            {t.todo}
+          </span>
+          <button onClick={() => handleEdit(t.id)}>Edit</button>
+          <button onClick={() => handleDelete(t.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default TodoList;
