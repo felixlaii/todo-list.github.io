6947f843fbe4c313ceab4510fad0ca8fd8d9d622
Felix Lai <felix.lai@hotmail.com>
2022-11-13 10:29:16 -0500

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,13 @@ import Reach from 'react';
 const TodoList = ({ todos, handleDelete, handleEdit }) => {
     return (
         <ul className="allTodos">
-            
+            {todos.map((t) => (
+                <li className='singleTodo'>
+                    <span className='todoText' key={t.id}>
+                        {t.todo}
+                    </span>
+                </li>
+            ))}
         </ul>
     )
 }
