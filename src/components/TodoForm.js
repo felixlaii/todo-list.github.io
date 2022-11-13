f2109d145c195d18c47a21c932f8e66ee0dd1fd4
Felix Lai <felix.lai@hotmail.com>
2022-11-13 10:31:32 -0500

    Update TodoForm.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 const TodoForm = ({ handleSubmit, todo, editId, setTodo }) => {
     return (
-        
+        <form className='todoForm' onSubmit={handleSubmit}>
+            <input
+            type='text'
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)} 
+            />
+        </form>
     )
 }
