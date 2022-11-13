9164da7591b5ab1b1fbe0fdf8e9666418ddcc9f0
Felix Lai <felix.lai@hotmail.com>
2022-11-13 10:33:14 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
     </div>
